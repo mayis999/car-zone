b23e3664f1d2d9130cc2504331e3ae8a8d8747c0
mayis999 <mayis.sargsyan@inbox.ru>
2024-01-16 22:44:15 +0400

    first commit

---
+++ pages/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    facebook_link = models.URLField(max_length=100)
+    twitter_link = models.URLField(max_length=100)
+    google_plus_link = models.URLField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
+
+
+
+
